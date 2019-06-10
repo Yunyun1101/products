f361f8bc6ba55d96884a2030f105e7e73beec11e
Vicky Yun <marsyunyun1101@gmail.com>
2019-06-10 14:53:16 +0800

    refactor main function

--- products.py
+++ products.py
@@ -1,19 +1,14 @@
 import os # operating system
-# products = []
 
-#檢查檔案是否有, 若有讀取檔案
+#讀取檔案
 def read_file(filename):
 	products = []
-	if os.path.isfile(filename): #找相對路徑,同樣的資料夾是否有檔案
-		print('yeah! 找到檔案了！')
-		with open(filename,'r',encoding='utf-16') as f:
-			for line in f:
-				if '商品,價格' in line:
-					continue # 繼續
-				name , price = line.strip().split(',') #strip 去除換行＆前後空格, split 切割前後字串
-				products.append([name, price])
-	else:
-		print('找不到檔案....')
+	with open(filename,'r',encoding='utf-16') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue # 繼續
+			name , price = line.strip().split(',') #strip 去除換行＆前後空格, split 切割前後字串
+			products.append([name, price])
 	return products
 
 # 讓使用者輸入
@@ -39,7 +34,16 @@ def write_file(filename, products):
 		for p in products:
 			f.write(p[0] + ',' + str(p[1]) + '\n')
 
-products = read_file('products.csv')
-products = user_input(products)
-print_products(products)
-write_file('products.csv',products)
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): #找相對路徑,同樣的資料夾是否有檔案
+		print('yeah! 找到檔案了！')
+		products = read_file(filename)
+	else:
+		print('找不到檔案....')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv',products)
+
+main()
